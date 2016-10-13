55c1512e408cd304b5a851cdd84a6d17ff4a3136
Tony Tsang <klsstsang@yahoo.com>
2016-10-13 14:59:47 +0800

    Add randomWeightedValue generate function to MathUtil together with WeightedRandomValueHolder & WeightedRandomValue classes

--- src/utils/math/MathUtil.kt
+++ src/utils/math/MathUtil.kt
@@ -1279,7 +1279,7 @@ object MathUtil {
     }
 
     /**
-     * General a random number array with given random generator for consistent result
+     * Generate a random number array with given random generator for consistent result
      *
      * @param min minimum bound of random value
      * @param max maximum bound of random value
@@ -1380,6 +1380,18 @@ object MathUtil {
         } while (v < min || v > max);
 
         return v
+    }
+
+    /**
+     * Generate a random value from weighted random value holder, see {@link utils.MathUtil.WeightedRandomValueHolder}
+     * 
+     * @param weightedRandomValueHolder weighted random value holder
+     * 
+     * @return generated random value
+     */
+    @JvmStatic
+    fun <E> randomWeightedValue(weightedRandomValueHolder: WeightedRandomValueHolder<E>): E {
+        return weightedRandomValueHolder.determineValue(randomInteger(1, weightedRandomValueHolder.totalWeight))
     }
 
     /**
@@ -3464,4 +3476,101 @@ object MathUtil {
             }
         }
     }
+
+    /**
+     * Weighted random value holder
+     *
+     */
+    class WeightedRandomValueHolder<E> {
+        /**
+         * total possible weight
+         * 
+         * setter is set to private to ensure consistency of total weight (inconsistent total weight will be adjusted during addWeightedRandomValue)
+         */
+        var totalWeight: Int
+            private set(value) {
+                field = value
+            }
+
+        /**
+         * default value to return, if generated weight value exceed all weight intervals
+         *
+         * This can be view as weighted value with weight = totalWeight - sum of other weighted value
+         */
+        val defaultValue: E
+
+        /**
+         * Weighted value list
+         */
+        val weightedRandomValueList = ArrayList<WeightedRandomValue<E>>()
+
+        /**
+         * Weight intervals to determine value with provided weight
+         */
+        private val weightIntervals = ArrayList<Int>()
+        
+        /**
+         * construct a WeightedRandomValueHolder
+         * 
+         * @param totalWeight total weight of the holder, if this is zero defaultValue will never be generated.
+         * @param defaultValue default value, value to return if generated weight value exceed all other weighted values
+         * @param weightedRandomValues all other weighted values
+         */
+        constructor(totalWeight: Int, defaultValue: E, vararg weightedRandomValues: WeightedRandomValue<E>) {
+            this.totalWeight = totalWeight
+            this.defaultValue = defaultValue
+            addWeightedRandomValues(*weightedRandomValues)
+        }
+
+        /**
+         * Add weighted values
+         * 
+         * @param values weighted values
+         */
+        fun addWeightedRandomValues(vararg values: WeightedRandomValue<E>) {
+            values.forEach { 
+                addWeightedRandomValue(it)
+            }
+        }
+
+        /**
+         * Add a weighted value
+         * 
+         * @param value weighted value
+         */
+        fun addWeightedRandomValue(value: WeightedRandomValue<E>) {
+            val currentIntervalWeight = weightIntervals.sum()
+            val newInterval = currentIntervalWeight + value.weight
+            
+            weightedRandomValueList.add(value)
+            weightIntervals.add(newInterval)
+            
+            // inconsistent total weight, set to newInterval value (if this happen default value will no longer be returned)
+            if (totalWeight < newInterval) {
+                totalWeight = newInterval
+            }
+        }
+
+        /**
+         * Determine the final random with weight value
+         * 
+         * @param value weight value
+         * @return determined value, if weight value exceed all intervals, default value will be returned 
+         */
+        fun determineValue(value: Int): E {
+            weightIntervals.forEachIndexed {
+                idx, interval -> 
+                if (value <= interval) {
+                    return weightedRandomValueList[idx].value
+                }
+            }
+            
+            return defaultValue
+        }
+    }
+
+    /**
+     * Random value with weight
+     */
+    data class WeightedRandomValue<E>(val value: E, val weight: Int)
 }
