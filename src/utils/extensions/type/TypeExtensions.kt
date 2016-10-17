98c8fea8619e7bebdc3208cf667cde5bb6b50ea6
Tony Tsang <klsstsang@yahoo.com>
2016-10-17 16:17:58 +0800

    Add extensions functions for Date
    - plus (int), minus (int) add days to receiver date
    - minus (Date), compute day difference of receiver date
    - inc, dec increment / decrement receiver date by 1
    - rangeTo (Date) generate date range by step 1
    - downTo negative range with step 1
    - DateProgression.step for step keyword

--- src/utils/extensions/type/TypeExtensions.kt
+++ src/utils/extensions/type/TypeExtensions.kt
@@ -1,7 +1,12 @@
 package utils.extensions.type
 
 import utils.data.DataManipulator
+
+import utils.date.DateProgression
+import utils.date.DateRange
 import utils.date.DateTimeParser
+import utils.date.SimpleDateUtils
+
 import utils.math.MathUtil
 import java.math.BigDecimal
 import java.util.*
@@ -58,4 +63,79 @@ fun <K, V> Map<K, V>.getDate(key: K, default: Date = Date()): Date {
 
 fun <K, V> Map<K, V>.getDateOrNull(key: K, default: Date? = Date()): Date? {
     return DataManipulator.getDate(this, key, default)
+}
+
+infix operator fun Date.plus(inc: Int): Date {
+    val cal = Calendar.getInstance()
+    
+    cal.timeInMillis = this.time
+    
+    cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR) + inc)
+    
+    return cal.time
+}
+
+infix operator fun Date.minus(dcr: Int): Date {
+    return this + -dcr
+}
+
+operator fun Date.inc(): Date {
+    return this + 1
+}
+
+operator fun Date.dec(): Date {
+    return this - 1
+}
+
+infix operator fun Date.rangeTo(other: Date): DateRange {
+    return DateRange(this, other)
+}
+
+operator fun Date.minus(other:Date): Int {
+    val thisCal = Calendar.getInstance()
+    val otherCal = Calendar.getInstance()
+    
+    thisCal.timeInMillis = this.time
+    otherCal.timeInMillis = other.time
+    
+    val thisYear = thisCal.get(Calendar.YEAR)
+    val otherYear = otherCal.get(Calendar.YEAR)
+    
+    val thisDayOfYear = thisCal.get(Calendar.DAY_OF_YEAR)
+    val otherDayOfYear = otherCal.get(Calendar.DAY_OF_YEAR)
+    
+    if (thisYear == otherYear) {
+        return thisDayOfYear - otherDayOfYear
+    } else if (thisYear > otherYear) {
+        var days = if (SimpleDateUtils.isLeapYear(otherYear)) (366-otherDayOfYear) else (365-otherDayOfYear)
+        
+        for (y in otherYear + 1 .. thisYear - 1) {
+            days += if (SimpleDateUtils.isLeapYear(y)) 366 else 365
+        }
+        
+        days += thisDayOfYear
+        
+        return days
+    } else {
+        var days = thisDayOfYear - if (SimpleDateUtils.isLeapYear(thisYear)) 366 else 365
+        
+        for (y in thisYear + 1 .. otherYear - 1) {
+            days -= if (SimpleDateUtils.isLeapYear(y)) 366 else 365
+        }
+        
+        days -= otherDayOfYear
+        
+        return days
+    }
+}
+
+infix fun DateProgression.step(step: Int): DateProgression {
+    if (step == 0) {
+        throw IllegalArgumentException("Step is zero.")
+    }
+    return DateProgression.fromClosedRange(first, last, if (this.step > 0) step else -step)
+}
+
+infix fun Date.downTo(to: Date): DateProgression {
+    return DateProgression.fromClosedRange(this, to, -1)
 }
