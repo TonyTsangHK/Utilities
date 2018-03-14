ca04d04cd7fb03ae8dc5f9bf484330b8b523f78b
Tony Tsang <klsstsang@yahoo.com>
2018-03-14 17:45:07 +0800

    Add ALPHANUMERICS constant characters to StringUtil

--- src/utils/string/StringUtil.java
+++ src/utils/string/StringUtil.java
@@ -43,7 +43,24 @@ public class StringUtil {
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         },
         HEX_CHARS   = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'},
-        DIGIT_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        DIGIT_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        ALPHANUMERICS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        },
+        UPPER_ALPHANUMERICS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        },
+        LOWER_ALPHANUMERICS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
     
     // Half & full with character code point difference, except space which is 12256
     // Half width character code point range:    33 ..   126
